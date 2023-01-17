587ad3d7cae57e09aed137e5775aab787f6a6e2b
Abror Mamataliev <abrornomos2018@gmail.com>
2023-01-17 22:57:20 +0500

    Small bug fix

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace ETickets
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddDbContext<AppDbContext>()
+            builder.Services.AddDbContext<AppDbContext>();
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
