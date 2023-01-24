e23686c4b1d44590cd426c9f1b544335381864c0
Abror Mamataliev <abrornomos2018@gmail.com>
2023-01-24 23:18:22 +0500

    Database connection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using ETickets.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ETickets
 {
@@ -9,7 +10,7 @@ namespace ETickets
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddDbContext<AppDbContext>();
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString")));
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
